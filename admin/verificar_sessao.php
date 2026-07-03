10aa53ca52d745995b2fa00880f19cb18d6cdf88
Beatriz <beatriz.kava@gmail.com>
2026-07-03 18:00:10 -0300

    Update/correct_login_update_2

--- admin/verificar_sessao.php
+++ admin/verificar_sessao.php
@@ -5,7 +5,7 @@ session_start();
 header("cache-control: no-store, no-cache, must-revalidate");
 header("pragma: no-cache");
 header("expires: 0");
-
+//trecho corrigido porque estavamos armazenando a sessa com o nome do usário ao invés do nome que verificamos na verdade, ai da conflito nessa inhaca
 if (!isset($_SESSION['admin_logado']) || $_SESSION['admin_logado'] !== true) {
     header("Location: login.php");
     exit();
