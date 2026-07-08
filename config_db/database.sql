cce33dca8e56d603aaa939465d9dfd0b22a28cad
Beatriz <beatriz.kava@gmail.com>
2026-07-08 12:15:16 -0300

    update/database

--- config_db/database.sql
+++ config_db/database.sql
@@ -1,79 +1,201 @@
-
-/* ===========================
-ADMINISTRADORES
-=========================== */
---CREATE TABLE `administradores` (
---    `id` INT NOT NULL AUTO_INCREMENT,
---    `usuario` VARCHAR(50) NOT NULL,
---    `senha` VARCHAR(100) NOT NULL,
---    PRIMARY KEY (`id`),
---    UNIQUE KEY `usuario` (`usuario`)
---)
-
--- SENHA: Sql@2026k
-
-
-/* ===========================
---SERVIÇOS
---=========================== */
---DROP TABLE IF EXISTS `servicos`;
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
 --
---CREATE TABLE `servicos` (
---    `id` INT NOT NULL AUTO_INCREMENT,
---    `nome_servicos` VARCHAR(45) NOT NULL,
---    PRIMARY KEY (`id`),
---    UNIQUE KEY `nome_UNIQUE` (`nome_servicos`)
---) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Host: 127.0.0.1    Database: berenice
+-- ------------------------------------------------------
+-- Server version	8.0.46
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 --
---INSERT INTO `servicos` (`id`, `nome_servicos`) VALUES
---(1, 'Logo Simples'),
---(2, 'Logo + Tipografia'),
---(3, 'Identidade Visual'),
---(4, 'Brand Kit Completo'),
---(5, 'Ícones para Destaques'),
---(6, 'Posts para Redes');
+-- Table structure for table `administradores`
+--
 
+DROP TABLE IF EXISTS `administradores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `administradores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(50) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `usuario` (`usuario`),
+  UNIQUE KEY `senha_UNIQUE` (`senha`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/* ===========================
-leads
-=========================== */
---DROP TABLE IF EXISTS `leads`;
+--
+-- Dumping data for table `administradores`
+--
 
---CREATE TABLE `leads` (
---    `id` INT NOT NULL AUTO_INCREMENT,
---    `nome` VARCHAR(100) NOT NULL,
---    `email` VARCHAR(256) NOT NULL,
---    `telefone` VARCHAR(14) NOT NULL,
---    `servico_id` INT NOT NULL,
---    `mensagem` TEXT NOT NULL,
---    PRIMARY KEY (`id`),
---    UNIQUE KEY `email` (`email`),
---    KEY `servicos_fk_idx` (`servico_id`),
---    CONSTRAINT `servicos_fk`
---        FOREIGN KEY (`servico_id`)
---        REFERENCES `servicos` (`id`)
---)
+LOCK TABLES `administradores` WRITE;
+/*!40000 ALTER TABLE `administradores` DISABLE KEYS */;
+INSERT INTO `administradores` VALUES (1,'admin','Sql@2026k');
+/*!40000 ALTER TABLE `administradores` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/* ===========================
-clientes
-=========================== */
---CREATE TABLE clientes (
---    id INT AUTO_INCREMENT PRIMARY KEY,
---    nome VARCHAR(100) NOT NULL,
---    email VARCHAR(150),
---    telefone VARCHAR(20),
---    instagram VARCHAR(100),
---    servico_id INT NOT NULL,
---    mensagem TEXT,
---    data_cadastro DATETIME NOT NULL,
---    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
---        ON UPDATE CURRENT_TIMESTAMP,
---    FOREIGN KEY (servico_id) REFERENCES servicos(id)
---);
+--
+-- Table structure for table `clientes`
+--
 
-/* ===========================
-Status Cliente
-=========================== */
---CREATE TABLE status_clientes (
---    id INT AUTO_INCREMENT PRIMARY KEY,
---    nome_status NOT NULL,
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `cpf` varchar(14) DEFAULT NULL,
+  `instagram` varchar(100) DEFAULT NULL,
+  `servico_id` int NOT NULL,
+  `sobre` text,
+  `proposta_url` varchar(255) DEFAULT NULL,
+  `data_cadastro` datetime NOT NULL,
+  `data_atualizacao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status_clientes` int NOT NULL DEFAULT '6',
+  PRIMARY KEY (`id`),
+  KEY `servico_id` (`servico_id`),
+  KEY `fk_clientes_status_idx` (`status_clientes`),
+  CONSTRAINT `clientes_ibfk_1` FOREIGN KEY (`servico_id`) REFERENCES `servicos` (`id`),
+  CONSTRAINT `fk_clientes_status` FOREIGN KEY (`status_clientes`) REFERENCES `status_cliente` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Rosangela','rosngela.kava@hotmail.com','41991135689',NULL,NULL,3,'aaaaaa',NULL,'2026-07-08 00:04:01','2026-07-08 13:15:32',5),(2,'Beatriz','beatriz.kava@gmail.com','41 99107 3771',NULL,NULL,5,NULL,NULL,'2026-07-07 22:26:01','2026-07-08 13:15:50',6),(3,'Dante','dante.will@gmail.com','41 95183975',NULL,NULL,3,NULL,NULL,'2026-07-08 10:20:08','2026-07-08 13:21:42',6),(4,'Noreh','noreh@gmail.com','41 999999999',NULL,NULL,3,NULL,NULL,'2026-07-08 10:25:20','2026-07-08 13:27:33',1);
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `leads`
+--
+
+DROP TABLE IF EXISTS `leads`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `leads` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `telefone` varchar(14) NOT NULL,
+  `servicos` int NOT NULL,
+  `mensagem` text NOT NULL,
+  `observacao` text,
+  `status_lead` int NOT NULL DEFAULT '1',
+  `data_clientes` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `servicos_fk_idx` (`servicos`),
+  KEY `status_fk_idx` (`status_lead`),
+  CONSTRAINT `servicos_fk` FOREIGN KEY (`servicos`) REFERENCES `servicos` (`id`),
+  CONSTRAINT `status_leads_fk` FOREIGN KEY (`status_lead`) REFERENCES `status_lead` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `leads`
+--
+
+LOCK TABLES `leads` WRITE;
+/*!40000 ALTER TABLE `leads` DISABLE KEYS */;
+INSERT INTO `leads` VALUES (12,'Manuel','manu@gmail.com',' 41 88154721',5,'Nnn',NULL,1,'2026-07-08 13:49:52');
+/*!40000 ALTER TABLE `leads` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicos`
+--
+
+DROP TABLE IF EXISTS `servicos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servicos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome_servicos` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nome_UNIQUE` (`nome_servicos`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicos`
+--
+
+LOCK TABLES `servicos` WRITE;
+/*!40000 ALTER TABLE `servicos` DISABLE KEYS */;
+INSERT INTO `servicos` VALUES (4,'Brand Kit Completo'),(5,'Ícones para Destaques'),(3,'Identidade Visual'),(2,'Logo + Tipografia'),(1,'Logo Simples'),(6,'Posts para Redes');
+/*!40000 ALTER TABLE `servicos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status_cliente`
+--
+
+DROP TABLE IF EXISTS `status_cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `status_cliente` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome_status` varchar(45) NOT NULL DEFAULT '6',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status_cliente`
+--
+
+LOCK TABLES `status_cliente` WRITE;
+/*!40000 ALTER TABLE `status_cliente` DISABLE KEYS */;
+INSERT INTO `status_cliente` VALUES (1,'Ativo'),(2,'Inativo'),(3,'Pausado'),(4,'Finalizado'),(5,'Aguardando pagamento'),(6,'Proposta enviada');
+/*!40000 ALTER TABLE `status_cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status_lead`
+--
+
+DROP TABLE IF EXISTS `status_lead`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `status_lead` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lead_status` varchar(45) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status_lead`
+--
+
+LOCK TABLES `status_lead` WRITE;
+/*!40000 ALTER TABLE `status_lead` DISABLE KEYS */;
+INSERT INTO `status_lead` VALUES (1,'Novo'),(2,'Em Contato'),(3,'Perdido');
+/*!40000 ALTER TABLE `status_lead` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-07-08 12:14:02
